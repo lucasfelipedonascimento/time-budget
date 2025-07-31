8b31af6a8ed7dafefa2a7c0aa56bdceb455a81fe
lucasfelipedonascimento <lucasfelipe3000@gmail.com>
2025-07-31 11:14:17 -0300

    test: update test file config

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,6 +12,8 @@ export default defineConfig({
       exclude: [
         'vitest.**.ts',
         'vitest.setup.**.ts',
+        'src/constants/**',
+        'src/contexts/**/**/dto'
       ]
     },
     environment: "node",
